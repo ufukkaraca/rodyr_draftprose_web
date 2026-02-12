8b9533b664a6ad566a8e2f79c4f02a080a1e4f8b
Ufuk Karaca <59178846+ufukkaraca@users.noreply.github.com>
2026-02-12 18:00:29 +0800

    another missing link to run on demo.drafprose.com (does not affect local deployment)

--- lib/auth.ts
+++ lib/auth.ts
@@ -3,6 +3,8 @@ import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "@/lib/prisma";
 
 export const auth = betterAuth({
+    baseURL: process.env.BETTER_AUTH_URL,
+    trustedOrigins: [process.env.NEXT_PUBLIC_APP_URL!],
     database: prismaAdapter(prisma, {
         provider: "postgresql", // or "mysql", "postgresql", ...etc
     }),
